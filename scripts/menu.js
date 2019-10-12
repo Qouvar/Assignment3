78b92e85cc50529d8a6b74aa04b523670201f4d3
Blanko2 <rodrigo.lomonaco@gmail.com>
2019-10-13 00:40:29 +1300

    testing out some menu stuff

--- scripts/menu.js
+++ scripts/menu.js
@@ -5,3 +5,9 @@ $(function() {
            at: "right top" }
     });
  });
+$(document).ready(function(){
+    var $toggleButton = $('.toggle-button');
+    $toggleButton.on('click', function(){
+        $(this).toggleClass('button-open');
+    });
+})
