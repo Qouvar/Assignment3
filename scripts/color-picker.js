b13e25c31fabb2fc6aef820f80e002c887ad3954
Qouvar <yantair@gmail.com>
2019-10-13 00:47:45 +1300

    colourpick

---
+++ scripts/color-picker.js
@@ -0,0 +1,43 @@
+$( function() {
+  function hexFromRGB(r, g, b) {
+    var hex = [
+      r.toString( 16 ),
+      g.toString( 16 ),
+      b.toString( 16 )
+    ];
+    $.each( hex, function( nr, val ) {
+      if ( val.length === 1 ) {
+        hex[ nr ] = "0" + val;
+      }
+    });
+    hexStr = hex.join( '' );
+    $('#hexcode').html('Hexadecimal code #'+hexStr);
+    return hexStr.toUpperCase();
+  }
+  function refreshSwatch() {
+    var red = $( "#red" ).slider( "value" ),
+      green = $( "#green" ).slider( "value" ),
+      blue = $( "#blue" ).slider( "value" ),
+      hex = hexFromRGB( red, green, blue );
+    $( "#swatch" ).css( "background-color", "#" + hex );
+  }
+
+
+  $( "#red, #green, #blue" ).slider({
+    orientation: "horizontal",
+    range: "min",
+    max: 255,
+    value: 127,
+    slide: refreshSwatch,
+    change: refreshSwatch
+  });
+  $( "#red" ).slider( "value", 43 );
+  $( "#green" ).slider( "value", 27 );
+  $( "#blue" ).slider( "value", 27 );
+} );
+
+//background colour picker
+function changeBodyBg(color){
+        document.body.style.backgroundColor = color;
+    }
+
