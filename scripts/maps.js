3579f41955e93a807a7308945399775e7334de65
Qouvar <yantair@gmail.com>
2019-10-16 03:23:39 +1300

    found a way to trick it into doing both google maps

--- scripts/maps.js
+++ scripts/maps.js
@@ -1,4 +1,34 @@
 function initMap() {
+//----places to see
+  var tosee = {lat: -43.8769338, lng: 170.3640974};
+  var church = {lat: -44.003397, lng: 170.482513};
+  var lupin = {lat: -44.016661, lng: 170.502775};
+  var centre = {lat: -43.733200, lng: 170.093535};
+
+  var map1 = new google.maps.Map(document.getElementById('map1'), {
+    zoom: 9.9,
+    center: tosee
+  });
+
+  var churchMarker = new google.maps.Marker({
+    position: church,
+    map: map1,
+    title: 'Church of the Good Shepherd',
+    label: 'A',
+  });
+  var lupinMarker = new google.maps.Marker({
+    position: lupin,
+    map: map1,
+    title: 'The Lupin Fields',
+    label: 'B',
+  });
+  var centreMarker = new google.maps.Marker({
+    position: centre,
+    map: map1,
+    title: 'Sir Edmund Hillary Alpine Centre',
+    label: 'C',
+  });    
+
   var stay = {lat: -43.7347512, lng: 170.0984613};
 
   var yha = {lat: -43.737406, lng: 170.100789};
@@ -29,53 +59,4 @@ function initMap() {
     title: 'The Hermitage Hotel',
     label: 'C',
   });
-
-
-
-
-
-
-
-        var tosee = {lat: -43.8769338, lng: 170.3640974};
-     
-
-        var church = {lat: -44.003397, lng: 170.482513};
-        var lupin = {lat: -44.016661, lng: 170.502775};
-        var centre = {lat: -43.733200, lng: 170.093535};
-
-
-
-      
-
-        var map1 = new google.maps.Map(document.getElementById('map1'), {
-          zoom: 9.9,
-          center: tosee
-        });
-        var map2 = new google.maps.Map(document.getElementById('map2'), {
-          zoom: 15.5,
-          center: stay
-        });
-        //----places to see markers
-        var churchMarker = new google.maps.Marker({
-          position: church,
-          map: map1,
-          title: 'Church of the Good Shepherd',
-          label: 'A',
-        });
-        var lupinMarker = new google.maps.Marker({
-          position: lupin,
-          map: map1,
-          title: 'The Lupin Fields',
-          label: 'B',
-        });
-        var centreMarker = new google.maps.Marker({
-          position: centre,
-          map: map1,
-          title: 'Sir Edmund Hillary Alpine Centre',
-          label: 'C',
-        });
-      
-
-
-        
-      }
+  }
