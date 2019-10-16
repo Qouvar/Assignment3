c2f2d07eb27d147db89ec31efa9afbda0192ba73
Blanko2 <rodrigo.lomonaco@gmail.com>
2019-10-16 14:23:35 +1300

    fixing issue with the map not appearing by splitting the map.js files so they actually run properly

--- scripts/maps.js
+++ scripts/maps.js
@@ -1,3 +1,5 @@
+var map1, map2;
+
 function initMap() {
 //----places to see
   var tosee = {lat: -43.8769338, lng: 170.3640974};
@@ -28,35 +30,4 @@ function initMap() {
     title: 'Sir Edmund Hillary Alpine Centre',
     label: 'C',
   });    
-
-  var stay = {lat: -43.7347512, lng: 170.0984613};
-
-  var yha = {lat: -43.737406, lng: 170.100789};
-  var alpine = {lat: -43.733837, lng: 170.095865};
-  var hermitage = {lat: -43.733090, lng: 170.093615};
-
-  var map2 = new google.maps.Map(document.getElementById('map2'), {
-    zoom: 15.5,
-    center: stay
-  });
-
-  //----accommodations
-  var yhaMarker = new google.maps.Marker({
-    position: yha,
-    map: map2,
-    title: 'YHA Aoraki Mt. Cook Backpackers',
-    label: 'A',
-  });
-  var alpineMarker = new google.maps.Marker({
-    position: alpine,
-    map: map2,
-    title: 'Aoraki Mt Cook Alpine Lodge',
-    label: 'B',
-  });
-  var hermitageMarker = new google.maps.Marker({
-    position: hermitage,
-    map: map2,
-    title: 'The Hermitage Hotel',
-    label: 'C',
-  });
-  }
+}
